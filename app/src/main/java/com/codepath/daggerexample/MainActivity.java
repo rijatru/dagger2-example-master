5f92876639d1e11bdb8f56fc625c52bc943d93e0
Ricardo <rijatru@gmail.com>
2016-02-29 13:19:00 -0500

    Initial commit

---
+++ app/src/main/java/com/codepath/daggerexample/MainActivity.java
@@ -0,0 +1,60 @@
+package com.codepath.daggerexample;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.codepath.daggerexample.network.interfaces.GitHubApiInterface;
+import com.squareup.okhttp.OkHttpClient;
+
+import javax.inject.Inject;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Inject
+    GitHubApiInterface mGitHubApiInterface;
+
+    @Inject
+    SharedPreferences mSharedPreferences;
+
+    @Inject
+    OkHttpClient mOkHttpClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ((MyApp) getApplication()).getGitHubComponent().inject(this);
+
+        Log.d("Dagger", mSharedPreferences.getAll().toString());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
